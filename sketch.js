cddfa82ec074283adabaa7d53367452926dff7b1
Krishnaarpit12 <67108304+Krishnaarpit12@users.noreply.github.com>
2020-09-03 10:06:50 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,18 +6,22 @@ var bullet,speed,weight;
 
 function setup() {
   createCanvas(1600,400);
-  bullet=createSprite(50, 200, 40, 15);
-  bullet.shapeColor = color("white");
-  wall= createSprite(1200, 200,thickness,height/2);
-  wall.shapeColor = color (80,80,80);
 
-  bullet.velocityX = random(223,321);
-  bullet.collide(wall);
- 
- 
   speed = random(223,321);
   weight = random(30,52)
   thickness = random(22,83)
+
+  bullet=createSprite(50, 200, 40, 15);
+  bullet.shapeColor = color("white");
+  
+  wall= createSprite(1200, 200,thickness,height/2);
+  wall.shapeColor = color (80,80,80);
+
+  bullet.velocityX = speed;
+  bullet.collide(wall);
+ 
+ 
+  
 }
 function draw() {
   background("yellow");  
